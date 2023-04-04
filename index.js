ce6b80feb55eeb5b064d43e7ddb52422c9b7084b
Aaron Sala <salaaron2@gmail.com>
2023-04-05 01:13:18 +0300

    simple calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+l
+num1=3
+num2=987
+
+
+
+function add(){
+  sum=num1+num2;
+  answer.innerHTML=sum;
+
+}
+function subtract(){
+  sum= num1-num2;
+  answer.innerHTML=sum;
+
+}
+function multiplye(){
+  sum=num1*num2;
+  answer.innerHTML=sum;
+
+}
+function divider(){
+  sum=num1/num2;
+  answer.innerHTML=sum;
+
+}
+https://www.youtube.com/watch?v=jS4aFq5-91M
+https://www.youtube.com/watch?v=P8YuWEkTeuE
