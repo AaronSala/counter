196f7ef6dc9fed5a82b89f56b2e177206f0deae7
Aaron Sala <salaaron2@gmail.com>
2023-04-05 01:52:39 +0300

    age checker before entering a club

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 
 
-
-l
-num1=3
-num2=987
-
+var input=document.getElementById('age');
+var result = document.getElementById('result');
+//var click=document.getElementById('click')
 
 
-function add(){
-  sum=num1+num2;
-  answer.innerHTML=sum;
+input.addEventListener('input', age());
 
+function results(){
+  if(input.value ==''){
+    result.innerHTML='please enter your age';
+  }
+else if(input.value< 18){
+  result.innerHTML='you are too young to be here';
 }
-function subtract(){
-  sum= num1-num2;
-  answer.innerHTML=sum;
-
+else if(input.value >18 && input.value<70){
+  result.innerHTML='you are welcome drink responsibly';
 }
-function multiplye(){
-  sum=num1*num2;
-  answer.innerHTML=sum;
-
+else{
+  result.innerHTML='you are too old to be here';
 }
-function divider(){
-  sum=num1/num2;
-  answer.innerHTML=sum;
-
 }
-https://www.youtube.com/watch?v=jS4aFq5-91M
-https://www.youtube.com/watch?v=P8YuWEkTeuE
+
