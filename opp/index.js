e769fdf60a4b668cac9f368c151feab691a3912e
Aaron Sala <salaaron2@gmail.com>
2023-09-20 14:32:15 +0300

    learning oop

--- opp/index.js
+++ opp/index.js
@@ -1,10 +1,7 @@
 
-
-const persons = document.querySelector('.names')
-const names = ['mutuku', 'mutua','mwengi'];
-
-names.forEach(person=>{
-    persons.setAttribute('style', 'margin:0px')
-    persons.innerHTML +=`<p style='font:bold'>${person}</p> `
+var fun = function(...num) {
+    console.log(num[0]*num[1]-num[2]);
     
-})
+}
+arr =[8, 9, 12]
+fun(... arr);
